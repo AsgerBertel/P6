233291edf26678aee6f834852345cee5a8b87884
asger <asgerb@hotmail.dk>
2020-03-05 11:57:29 +0100

    DataUnzipper merged into P6

---
+++ src/main/java/TweetCleaner/JsonCleaner.java
@@ -0,0 +1,24 @@
+package TweetCleaner;
+
+public class JsonCleaner {
+
+
+    /*
+    Structure of a regular tweet:
+    {
+        "created_at": "Thu May 10 15:24:15 +0000 2018",
+            "id_str": "850006245121695744",
+            "text": "Here is the Tweet message.",
+            "user": {
+    },
+        "place": {
+    },
+        "entities": {
+    },
+        "extended_entities": {
+    }
+    }
+
+     */
+
+}
