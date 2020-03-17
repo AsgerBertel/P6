c1bdc126000e69a60abae6a0ac9169b06ef3c8c3
Mads <mk96@live.dk>
2020-03-17 11:48:16 +0100

    small changes and refactoring

--- src/main/java/DataUnzipper/UnZipper.java
+++ src/main/java/DataUnzipper/UnZipper.java
@@ -3,26 +3,17 @@ package DataUnzipper;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.zip.GZIPInputStream;
 
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.apache.commons.compress.utils.IOUtils;
 
-public class ZipManager {
+public class UnZipper {
 
-    public static void unGzip(final File inputFile, final File outputDir, File renamedFile, File tempDir) throws IOException {
-        if (inputFile.getName().contains("bz2")) {
-            unZipBz(inputFile, outputDir, renamedFile,tempDir);
-        } else {
-            unTar(inputFile, outputDir);
-        }
-    }
-
-    private static void unZipBz(final File inputFile, final File outputDir, File renamedFile, File tempFileDir) throws IOException {
+    public static void unZipBz(final File inputFile, File renamedFile, File tempFileDir, String threadName) throws IOException {
         //System.out.println(String.format("Ungzipping %s to dir %s.", inputFile.getAbsolutePath(), outputDir.getAbsolutePath()));
-        final File outputFile = new File(tempFileDir,inputFile.getName());
+        final File outputFile = new File(tempFileDir,threadName + ".bz");
         InputStream in2 = Files.newInputStream(Paths.get(inputFile.toString()));
         final FileOutputStream out = new FileOutputStream(outputFile);
         BZip2CompressorInputStream in = new BZip2CompressorInputStream(in2);
@@ -36,7 +27,7 @@ public class ZipManager {
         }
     }
 
-    private static void unTar(final File inputFile, final File outputDir) throws IOException {
+    public static void unTar(final File inputFile, final File outputDir) throws IOException {
         FileInputStream fis = new FileInputStream(inputFile);
         TarArchiveInputStream tis = new TarArchiveInputStream(fis);
         TarArchiveEntry tarEntry = null;
