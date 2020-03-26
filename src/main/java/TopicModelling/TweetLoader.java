5aad5cac4afcce61fb64af1e2fca72d1f6d3d351
Mads <mk96@live.dk>
2020-03-26 14:34:52 +0100

    added additional filtering to clean tweets further. Now removes all contractions of useless words (don't, won't etc) and numerous symbols

--- src/main/java/TopicModelling/TweetLoader.java
+++ src/main/java/TopicModelling/TweetLoader.java
@@ -2,11 +2,12 @@ package TopicModelling;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
 
 public class TweetLoader {
+    private ArrayList<TopicModelTweet> tweets = new ArrayList<>();
 
     public ArrayList<TopicModelTweet> getTweetsFromFile(String path) throws IOException {
-        ArrayList<TopicModelTweet> tweets = new ArrayList<>();
         BufferedReader br = new BufferedReader(new FileReader(path));
         String line;
         while((line = br.readLine())!=null){
@@ -16,22 +17,49 @@ public class TweetLoader {
             }catch (IndexOutOfBoundsException e){
                 System.out.println(e + " :: " + line);
             }
-            if(tweets.size() > 50000){
+            /*if(tweets.size() > 10000){
                 break;
-            }
+            }*/
         }
+        return tweets;
+    }
+    public ArrayList<TopicModelTweet> getTweets(){
         return tweets;
     }
     private String cleanTweet(String text){
         //remove urls
-        if(text.contains("https")) {
+        if(text.contains("https"))
             text = text.replaceAll("https:\\S*"," ");
-        }
         //remove Retweet letters RT + @UsrName
-        if(text.contains("RT @")){
+        if(text.contains("RT @"))
             text = text.replaceAll("RT @\\S*", "");
-        }
-        //remove
+        //remove symbols
+        if(text.contains("&amp") || text.contains("&gt"))
+            text=text.replaceAll("&\\S*","");
+        if(text.contains("."))
+            text=text.replaceAll("\\.","");
+        //remove useless words
+        text = removeApostropheWords(text);
+        text = removeStopWords(text);
+        text = removeSymbols(text);
+        return text.trim();
+    }
+
+    private String removeSymbols(String text) {
+        Matcher matcher = SymbolAndWordRemovePatterns.symbolsPattern.matcher(text);
+        text = matcher.replaceAll(" ");
+        return text;
+    }
+
+    private String removeApostropheWords(String text) {
+        Matcher matcher = SymbolAndWordRemovePatterns.apostropheWordPattern.matcher(text);
+        text = matcher.replaceAll("");
+        return text;
+    }
+
+    private String removeStopWords(String text){
+        Matcher matcher = SymbolAndWordRemovePatterns.stopWordPattern.matcher(text);
+        text = matcher.replaceAll("");
         return text;
     }
 }
