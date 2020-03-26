44e1df45d60258e63932de4c075f21b7ebd463c8
asger <asgerb@hotmail.dk>
2020-03-26 11:40:44 +0100

    testcommit

--- src/main/java/Sql/QueryManager.java
+++ src/main/java/Sql/QueryManager.java
@@ -5,8 +5,9 @@ public class QueryManager {
     public static String connectionString = "jdbc:postgresql://62.107.118.55:5432/cubefrequency?user=postgres&password=password";
     public static String selectDateFromTweet = "SELECT date FROM cube.tweets ";
     public static String selectCoordinatesFromTweet = "SELECT lat,long FROM cube.tweets WHERE cube.tweets.tweetid > 10000000";
-    public static String selectAllFromTweet = "SELECT product,lat,long,opinion,date FROM cube.tweets Where tweetid < 4000000";
+    public static String selectAllFromTweet = "SELECT product,lat,long,opinion,date FROM cube.tweets Where tweetid > 2000000";
     public static String selectProductIDFromProduct = "SELECT lat,long FROM cube.tweets ";
+
 
     public static String insertIntoDate(int day, int month, int year) {
         return "INSERT INTO cube.datedimenson (year,month,day)  VALUES ('" + year + "', '" + month + "', '" + day + "')";
