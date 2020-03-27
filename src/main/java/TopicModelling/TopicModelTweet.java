f36c7b635570683dba2cafc523de7b6d72732df9
Mads <mk96@live.dk>
2020-03-27 14:11:34 +0100

    added arraylist to hold assigned topics. added getter and add method for topic list

--- src/main/java/TopicModelling/TopicModelTweet.java
+++ src/main/java/TopicModelling/TopicModelTweet.java
@@ -5,13 +5,19 @@ import java.util.ArrayList;
 public class TopicModelTweet {
     String id, modified_text, original_text;
     Sentiment sentiment;
-
-    ArrayList<String> topics = new ArrayList<>();
-
+    private ArrayList<String> topics = new ArrayList<>();
     public TopicModelTweet(String id, String modified_text, Sentiment sentiment) {
         this.id = id;
         this.modified_text = modified_text;
         this.sentiment = sentiment;
+    }
+
+    public ArrayList<String> getTopics() {
+        return topics;
+    }
+
+    public void addTopic(String s){
+        topics.add(s);
     }
 
     public TopicModelTweet(String id, String modified_text, String original_text) {
