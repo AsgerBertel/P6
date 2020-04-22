1681ac15fd5a82ca6a8711fe0ea55e975eb4a54d
mads faber <madsfaber111@live.dk>
2020-04-22 11:22:48 +0200

    added ja fx library

--- src/main/java/GUI/Scene/Scene.java
+++ src/main/java/GUI/Scene/Scene.java
@@ -1,4 +1,15 @@
 package GUI.Scene;
 
-public class Scene {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Scene extends Application {
+
+
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
