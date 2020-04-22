a26f2a0f4333e8539f296e3e61a90493009af605
mads faber <madsfaber111@live.dk>
2020-04-22 13:09:36 +0200

    created style

--- src/main/java/GUI/Scene/MainWindow.java
+++ src/main/java/GUI/Scene/MainWindow.java
@@ -1,13 +1,9 @@
 package GUI.Scene;
 
-import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
@@ -18,9 +14,10 @@ public class MainWindow extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root =  FXMLLoader.load(getClass().getResource("/MainScene.fxml"));
+        Parent root =  FXMLLoader.load(getClass().getResource("/FXML/MainScene.fxml"));
         stage.setTitle("P6");
         Scene scene = new Scene(root);
+        
         stage.setScene(scene);
         stage.show();
     }
