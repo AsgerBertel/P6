fb403dd9ab443b9297baacde307f8045eae5ac56
mads faber <madsfaber111@live.dk>
2020-04-22 12:23:15 +0200

    fixed fxml loader

--- src/main/java/GUI/Scene/MainWindow.java
+++ src/main/java/GUI/Scene/MainWindow.java
@@ -1,21 +1,24 @@
 package GUI.Scene;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import org.apache.hadoop.yarn.webapp.hamlet.Hamlet;
+
 
 public class MainWindow extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage stage) throws Exception {
-
-        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        Parent root =  FXMLLoader.load(getClass().getResource("/MainScene.fxml"));
         stage.setTitle("test");
         Scene scene = new Scene(root);
         stage.setScene(scene);
